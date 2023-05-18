17d099b0b891f65ac5a6aeb61a33966be70c6e02
Prakash sahu <sahuprakash643@gmail.com>
2023-05-18 14:40:02 +0530

    day 2

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -6,6 +6,20 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:[true,"Please enter your name"],
         maxLength:[25, "Name should less then 35 letters"],
-        minLength:[5,"Name should grater then 5 letters"]
+        minLength:[5,"Name should greater then 5 letters"]
+    },
+    email:{
+        type:String,
+        required:[true,"Please enter your email"],
+        unique:true,
+        validate:[validator.isEmail,"Please enter a valid email"]
+      
+    },
+    password:{
+        type:String,
+        required:[true,"Please enter your password"],
+        minLength:[6,"Password should be greater than 6 letters "],
+        seleect:false
+       
     },
 })
